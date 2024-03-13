515f8f527dbfee09ea17bb180da1b67ef96faf71
Fabriciomadusouza <126177779+Fabriciomadusouza@users.noreply.github.com>
2024-03-13 10:14:05 -0300

    Create inactivating.sh

---
+++ inactivating.sh
@@ -0,0 +1,10 @@
+#inativar terminal 
+#acesso-privilégio
+sudo su
+#limpar cache e config Chrome 
+rm -r .cache/google-chrome && rm -r .config/google-chrome
+#limpar cache e config Chromium
+#rm -rf ~/.cache/chromium && rm -rf ~/.config.chromium
+#remove token 
+rm -f /opt/videosoft/vs-os-interface/log/_database_token-recovery.log
+echo "*****************Inativação Concluida*************************"
